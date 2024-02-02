dfa99031157720c291aa98377234b844f228d5f6
elkomysara <sa1244@fayoum.edu.eg>
2024-02-03 00:44:42 +0200

    commit

--- main.h
+++ main.h
@@ -2,10 +2,31 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
 #include <unistd.h>
 
-int _printf(const char *format, ...);
-int _putchar(char c);
-int _puts(char *str);
 
-#endif /* MAIN_H */
+
+/**
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
+int printf_char(va_list val);
+int printf_string(va_list val);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
