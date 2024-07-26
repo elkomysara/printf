9f9aa531268d26d59db9fad4cdcc24dacb75a3cf
elkomysara <sa1244@fayoum.edu.eg>
2024-07-26 16:48:20 +0300

    alx-low_level_programming/0x0D-preprocessor/0-putchar

--- _puts.c
+++ _puts.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _puts - Writes a string to the buffer
+ * @str: The string to write
+ * @buffer: The buffer to write to
+ * @index: The current index in the buffer
+ * Return: The number of characters written
+ */
 int _puts(char *str, char *buffer, int *index)
 {
     int i;
