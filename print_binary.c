840a6704e5f8e66b053c8f4a6eafa0ed924ceb09
elkomysara <sa1244@fayoum.edu.eg>
2024-07-26 15:23:33 +0300

    alx-low_level_programming/0x0D-preprocessor/0-putchar

--- print_binary.c
+++ print_binary.c
@@ -1,45 +1,44 @@
 #include "main.h"
 
 /**
-* print_binary - Converts an unsigned int argument to binary
-* @list: list of arguments
-* Return: The number of characters printed
-*/
+ * print_binary - Converts an unsigned int argument to binary
+ * @list: list of arguments
+ * Return: The number of characters printed
+ */
 int print_binary(va_list list)
 {
-unsigned int n = va_arg(list, unsigned int);
-int num_chars = 0;
-char *binary_str;
+    unsigned int n = va_arg(list, unsigned int);
+    char *binary_str;
+    int num_chars;
 
-binary_str = convert(n, 2, 0);
-if (binary_str == NULL)
-return (-1);
-num_chars = _puts(binary_str);
-free(binary_str);
-return (num_chars);
+    binary_str = convert(n, 2, 0);
+    if (binary_str == NULL)
+        return (-1);
+
+    num_chars = _puts(binary_str);
+    return (num_chars);
 }
 
 /**
-* convert - Converts a number to a string in the given base
-* @num: The number to be converted
-* @base: The base to convert to
-* @lowercase: Flag to determine if hex letters should be lowercase
-* Return: A pointer to the converted string
-*/
+ * convert - Converts a number to a string in the given base
+ * @num: The number to be converted
+ * @base: The base to convert to
+ * @lowercase: Flag to determine if hex letters should be lowercase
+ * Return: A pointer to the converted string (static buffer)
+ */
 char *convert(unsigned long int num, int base, int lowercase)
 {
-static char *rep;
-static char buffer[50];
-char *ptr;
+    static char buffer[50];
+    char *ptr;
+    char *rep = (lowercase) ? "0123456789abcdef" : "0123456789ABCDEF";
 
-rep = (lowercase) ? "0123456789abcdef" : "0123456789ABCDEF";
-ptr = &buffer[49];
-*ptr = '\0';
+    ptr = &buffer[49];
+    *ptr = '\0';
 
-do {
-*--ptr = rep[num % base];
-num /= base;
-} while (num != 0);
+    do {
+        *--ptr = rep[num % base];
+        num /= base;
+    } while (num != 0);
 
-return (ptr);
+    return (ptr);
 }
