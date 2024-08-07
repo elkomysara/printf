cdd8dc3cceac5c70349cfb6ace9e9d57ccc20a08
elkomysara <sa1244@fayoum.edu.eg>
2024-08-07 23:46:56 +0300

    alx-low_level_programming/0x0D-preprocessor/0-putchar

--- format_reciever.c
+++ format_reciever.c
@@ -9,7 +9,6 @@
 * Return: The number of characters printed
 */
 
-
 int format_reciever(const char *format, conver_t f_list[], va_list arg_list)
 {
 int i, j, printed_chars = 0;
@@ -18,31 +17,25 @@ for (i = 0; format[i] != '\0'; i++)
 {
 if (format[i] == '%')
 {
-int found = 0;
 for (j = 0; f_list[j].sym != NULL; j++)
 {
 if (format[i + 1] == f_list[j].sym[0])
 {
 printed_chars += f_list[j].f(arg_list);
 i++;
-found = 1;
 break;
 }
 }
-if (!found)
+if (f_list[j].sym == NULL && format[i + 1] != ' ' && format[i + 1] != '\0')
 {
-if (format[i + 1] != '\0')
-{
-_putchar(format[i]);
+_putchar('%');
 _putchar(format[i + 1]);
 printed_chars += 2;
 i++;
 }
-else
+else if (f_list[j].sym == NULL && format[i + 1] == '\0')
 {
-_putchar(format[i]);
-printed_chars++;
-}
+return (-1);
 }
 }
 else
