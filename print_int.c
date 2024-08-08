c1c774295cc3b65b9722382aa7063b703b379a3b
elkomysara <sa1244@fayoum.edu.eg>
2024-08-08 04:17:06 +0300

    alx-low_level_programming/0x0D-preprocessor/0-putchar

--- print_int.c
+++ print_int.c
@@ -12,21 +12,23 @@ int num = va_arg(list, int);
 char buffer[12];
 int i = 0, len = 0, negative = 0;
 
-if (num < 0)
-{
+if (num == INT_MIN) {
+_putchar('-');
+_putchar('2');
+num = 147483648; // INT_MIN = -2147483648, so we need to handle 147483648
+negative = 2; // Count '-' and '2'
+} else if (num < 0) {
 _putchar('-');
 num = -num;
 negative = 1;
 }
 
-if (num == 0)
-{
+if (num == 0) {
 _putchar('0');
 return (1);
 }
 
-while (num != 0)
-{
+while (num != 0) {
 buffer[i++] = (num % 10) + '0';
 num /= 10;
 }
