6fac61200f30953b116792b72b5f5f4ecc3f9352
elkomysara <sa1244@fayoum.edu.eg>
2024-08-10 19:56:38 +0300

    alx-low_level_programming/0x0D-preprocessor/0-putchar

--- format_reciever.c
+++ format_reciever.c
@@ -8,6 +8,7 @@
 * @arg_list: Argument list
 * Return: The number of characters printed
 */
+
 int format_reciever(const char *format, conver_t f_list[], va_list arg_list)
 {
 int i, j, printed_chars = 0;
@@ -16,23 +17,25 @@ for (i = 0; format[i] != '\0'; i++)
 {
 if (format[i] == '%')
 {
-int found = 0;
 for (j = 0; f_list[j].sym != NULL; j++)
 {
 if (format[i + 1] == f_list[j].sym[0])
 {
 printed_chars += f_list[j].f(arg_list);
 i++;
-found = 1;
 break;
 }
 }
-if (!found)
+if (f_list[j].sym == NULL && format[i + 1] != ' ' && format[i + 1] != '\0')
 {
-_putchar(format[i]);   /* print the % */
-_putchar(format[i + 1]); /* print the unknown specifier */
+_putchar(format[i]);
+_putchar(format[i + 1]);
 printed_chars += 2;
 i++;
+}
+else if (f_list[j].sym == NULL && format[i + 1] == '\0')
+{
+return (-1);
 }
 }
 else
--- tests/main.c
+++ tests/main.c
@@ -6,25 +6,29 @@
 *
 * Return: Always 0
 */
+
 int main(void)
 {
 int len1, len2;
 
-len1 = _printf("Hello %%\n");
-len2 = printf("Hello %%\n");
+len1 = _printf(NULL);
+len2 = printf(NULL);
 printf("Returned: _printf: %d, printf: %d\n", len1, len2);
 
-len1 = _printf("%s\n", "This is a string");
-len2 = printf("%s\n", "This is a string");
+len1 = _printf("Hello %%");
+len2 = printf("Hello %%");
 printf("Returned: _printf: %d, printf: %d\n", len1, len2);
 
-len1 = _printf("%c\n", 'A');
-len2 = printf("%c\n", 'A');
+len1 = _printf("%s", NULL);
+len2 = printf("%s", NULL);
 printf("Returned: _printf: %d, printf: %d\n", len1, len2);
 
-/* Testing unknown specifier */
-len1 = _printf("Unknown: [%r]\n");
-len2 = printf("Unknown: [%%r]\n");
+len1 = _printf("%%");
+len2 = printf("%%");
+printf("Returned: _printf: %d, printf: %d\n", len1, len2);
+
+len1 = _printf("Unsupported specifier: %q\n");
+len2 = printf("Unsupported specifier: %q\n");
 printf("Returned: _printf: %d, printf: %d\n", len1, len2);
 
 return (0);
