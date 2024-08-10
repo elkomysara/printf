c1051c5fb1675c2a4cafe4f8b5fac7304f9894c1
elkomysara <sa1244@fayoum.edu.eg>
2024-08-10 23:32:20 +0300

    alx-low_level_programming/0x0D-preprocessor/0-putchar

--- format_reciever.c
+++ format_reciever.c
@@ -1,18 +1,20 @@
 #include "main.h"
 
 /**
- * format_reciever - Receives the format and argument list,
- * then calls the appropriate function.
- * @format: The format string containing the specifiers.
- * @f_list: List of conversion specifiers and their associated functions.
- * @arg_list: List of arguments to be formatted.
- * @buffer: The buffer to store the formatted output.
- * @index: The current index in the buffer.
- * Return: The total number of characters printed.
- */
+* format_reciever - Receives the main string and all the necessary parameters to
+* print a formatted string
+* @format: A string containing all the desired characters
+* @f_list: A list of conversion specifiers
+* @arg_list: A list containing all the arguments passed to the program
+* @buffer: The buffer to write to
+* @index: The current index in the buffer
+* Return: A total count of the characters printed
+*/
 int format_reciever(const char *format, conver_t f_list[], va_list arg_list, char *buffer, int *index)
 {
 int i, j, printed_chars = 0;
+int (*func)(va_list, char *, int *);
+
 for (i = 0; format[i] != '\0'; i++)
 {
 if (format[i] == '%')
@@ -21,23 +23,18 @@ for (j = 0; f_list[j].sym != NULL; j++)
 {
 if (format[i + 1] == f_list[j].sym[0])
 {
-printed_chars += f_list[j].f(arg_list, buffer, index);
-i++;
+func = f_list[j].f;
+printed_chars += func(arg_list, buffer, index);
 break;
 }
 }
-if (f_list[j].sym == NULL && format[i + 1] != ' ' && format[i + 1] != '\0')
+if (f_list[j].sym == NULL && format[i + 1] != ' ')
 {
 _putchar(format[i], buffer, index);
 _putchar(format[i + 1], buffer, index);
 printed_chars += 2;
+}
 i++;
-}
-
-else if (f_list[j].sym == NULL && format[i + 1] == '\0')
-{
-return (-1);
-}
 }
 else
 {
